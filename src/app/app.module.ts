2bbea40604b6a1d49596cdd874dff77147aa4b5d
Francois D'Agostini <francois.dagostini@gmail.com>
2018-05-01 21:26:57 +0200

    basic bulma layout done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
+import { TilesComponent } from './tiles/tiles.component';
 
+
+const appRoutes: Routes = [
+  {path: 'tiles', component: TilesComponent},
+  {path: '', component: HeroComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    TilesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: false} // <-- debugging purposes only
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent]
